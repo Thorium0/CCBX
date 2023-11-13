674fbbb848875ee05294b7bd126f8818f3674044
FoxSGR <foxsgrno1@gmail.com>
2023-11-13 13:14:11 +0000

    Move items from the controller to the silo

--- src/main/java/de/presti/ccbx/ccbx/CCBallistiXBlock.java
+++ src/main/java/de/presti/ccbx/ccbx/CCBallistiXBlock.java
@@ -1,15 +1,13 @@
 package de.presti.ccbx.ccbx;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
-import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.material.Material;
 import org.jetbrains.annotations.NotNull;
 
@@ -33,5 +31,26 @@ public class CCBallistiXBlock extends Block implements EntityBlock {
     @Override
     public BlockEntity newBlockEntity(@NotNull BlockPos pos, @NotNull BlockState state) {
         return Registration.CC_TILEENTITY.get().create(pos, state);
+    }
+
+    /**
+     * Allows the block entity to tick.
+     *
+     * @param level The level
+     * @param state The state
+     * @param type  The type
+     * @param <T>   The type
+     * @return The ticker
+     */
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(
+            @NotNull Level level,
+            @NotNull BlockState state,
+            @NotNull BlockEntityType<T> type
+    ) {
+        return type == Registration.CC_TILEENTITY.get() && !level.isClientSide
+                ? (l, p, s, t) -> ((CCBallistiXTileEntity) t).tick()
+                : null;
     }
 }
--- src/main/java/de/presti/ccbx/ccbx/CCBallistiXTileEntity.java
+++ src/main/java/de/presti/ccbx/ccbx/CCBallistiXTileEntity.java
@@ -1,17 +1,38 @@
 package de.presti.ccbx.ccbx;
 
+import ballistix.common.tile.TileMissileSilo;
 import dan200.computercraft.api.peripheral.IPeripheral;
+import electrodynamics.prefab.tile.components.ComponentType;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.ItemStackHandler;
 import org.jetbrains.annotations.NotNull;
 
 import static dan200.computercraft.shared.Capabilities.CAPABILITY_PERIPHERAL;
 
 public class CCBallistiXTileEntity extends BlockEntity {
+
+    /**
+     * A counter to do operations every few ticks.
+     */
+    private int tickCounter = 0;
+
+    /**
+     * The inventory of the block.
+     */
+    private final ItemStackHandler inventory = new CCBXItemStackHandler(this);
+
+    /**
+     * The capability of the inventory.
+     */
+    private final LazyOptional<ItemStackHandler> inventoryCap = LazyOptional.of(() -> inventory);
 
     public CCBallistiXTileEntity(BlockPos pos, BlockState state) {
         super(Registration.CC_TILEENTITY.get(), pos, state);
@@ -22,6 +43,38 @@ public class CCBallistiXTileEntity extends BlockEntity {
      */
     protected CCBallistiXPeripheral peripheral = new CCBallistiXPeripheral(this);
     private LazyOptional<IPeripheral> peripheralCap;
+
+    /**
+     * Loads the inventory.
+     *
+     * @param nbt The tag to load from
+     */
+    @Override
+    public void load(@NotNull CompoundTag nbt) {
+        super.load(nbt);
+
+        try {
+            var data = nbt.getCompound("Buffer");
+            inventory.deserializeNBT(data.getCompound("Inventory"));
+        } catch (Exception e) {
+            // Ignore, just log
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Saves the inventory.
+     *
+     * @param nbt The tag to save to
+     */
+    @Override
+    protected void saveAdditional(@NotNull CompoundTag nbt) {
+        super.saveAdditional(nbt);
+
+        var data = new CompoundTag();
+        data.put("Inventory", inventory.serializeNBT());
+        nbt.put("Buffer", data);
+    }
 
     /**
      * When a computer modem tries to wrap our block, the modem will call getCapability to receive our peripheral.
@@ -36,6 +89,77 @@ public class CCBallistiXTileEntity extends BlockEntity {
             }
             return peripheralCap.cast();
         }
+
+        if (cap == ForgeCapabilities.ITEM_HANDLER) {
+            return inventoryCap.cast();
+        }
+
         return super.getCapability(cap, direction);
+    }
+
+    /**
+     * Moves the items from our inventory to the missile silo above us every 10 ticks.
+     */
+    public void tick() {
+        tickCounter++;
+
+        // Only move items every 10 ticks and if there is something to move
+        if (
+                tickCounter % 10 != 0 ||
+                        (inventory.getStackInSlot(0).isEmpty() && inventory.getStackInSlot(1).isEmpty())
+        ) {
+            return;
+        }
+
+        var silo = getMissileSilo();
+        if (silo == null) {
+            return;
+        }
+
+        var inventoryComponent = silo.getComponent(ComponentType.Inventory);
+        if (inventoryComponent == null) {
+            return;
+        }
+
+        inventoryComponent.getCapability(ForgeCapabilities.ITEM_HANDLER, Direction.UP)
+                .ifPresent(wrapper -> {
+                    // Move the items from our inventory to the missile silo above us
+                    var ourMissileStack = inventory.getStackInSlot(0);
+                    var ourExplosiveStack = inventory.getStackInSlot(1);
+                    var theirMissileStack = wrapper.getStackInSlot(0);
+                    var theirExplosiveStack = wrapper.getStackInSlot(1);
+
+                    if (theirMissileStack.isEmpty() || ourMissileStack.getItem() == theirMissileStack.getItem()) {
+                        var newStack = wrapper.insertItem(0, ourMissileStack, false);
+                        inventory.setStackInSlot(0, newStack);
+                    }
+                    if (theirExplosiveStack.isEmpty() || ourExplosiveStack.getItem() == theirExplosiveStack.getItem()) {
+                        var newStack = wrapper.insertItem(1, ourExplosiveStack, false);
+                        inventory.setStackInSlot(1, newStack);
+                    }
+                });
+    }
+
+    /**
+     * Get the missile silo above this block.
+     *
+     * @return The missile silo above this block or null if there is none
+     */
+    private TileMissileSilo getMissileSilo() {
+        Level level = getLevel();
+        if (level == null) {
+            return null;
+        }
+
+        BlockEntity blockEntity = level.getBlockEntity(getBlockPos().above());
+        if (blockEntity == null) {
+            return null;
+        }
+
+        if (blockEntity instanceof TileMissileSilo tileMissileSilo) {
+            return tileMissileSilo;
+        }
+
+        return null;
     }
 }
